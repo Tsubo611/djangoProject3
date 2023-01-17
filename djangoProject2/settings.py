78453f953c00eeb674ca639ab1d9a964a30ac063
tsubo <tsubota116@gmail.com>
2023-01-17 16:57:56 -0500

    initial commit

--- djangoProject2/settings.py
+++ djangoProject2/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
 }
 """
 DATABASES = {
-        'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/st3539-webapps1',
+        'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/st3539-webapp-class-01',
                                           conn_max_age=600)}
 
 
